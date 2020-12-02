186afa89142a89173c0351405cd5f3c5dac6b9a3
David Sequera <davidsequerads@gmail.com>
2020-12-01 21:53:11 -0500

    Build included

---
+++ build/precache-manifest.0da21f3dad7e96d2ce29c18959e6c028.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf7084126e2bedd86b11f1c3e65a20e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb2bc1d33069e8f7115",
+    "url": "/static/css/main.81982cf8.chunk.css"
+  },
+  {
+    "revision": "353ebdafa67fb32b16fa",
+    "url": "/static/js/2.152099ca.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.152099ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb2bc1d33069e8f7115",
+    "url": "/static/js/main.6a7afd44.chunk.js"
+  },
+  {
+    "revision": "9933fe05e0499981be31",
+    "url": "/static/js/runtime-main.5349271a.js"
+  },
+  {
+    "revision": "785d5fdce1eb8ca1f623823b1b06e9ce",
+    "url": "/static/media/11.785d5fdc.mp4"
+  },
+  {
+    "revision": "411051cb61bcda5517943601e8734cea",
+    "url": "/static/media/Monoton-Regular.411051cb.ttf"
+  },
+  {
+    "revision": "5b76e96ef86855fdcf26aa6b1f61308e",
+    "url": "/static/media/round_logo.5b76e96e.png"
+  }
+]);
